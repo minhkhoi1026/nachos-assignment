a763c2ee33c0e5e5af781a42ccd02b10c767c242
minhkhoi1026 <minhkhoi1026@gmail.com>
2021-10-10 01:58:57 +0700

    complete help program

---
+++ nachos/NachOS-4.0/code/test/help.c
@@ -0,0 +1,17 @@
+#include "syscall.h"
+
+int main() {
+    PrintString("-----------Help program-----------\n");
+    PrintString("---Team member---\n");
+    PrintString("1. Nguyen Nhat Minh Khoi - 19120020\n");
+    PrintString("2. Bach Ngoc Minh Tam - 19120034\n");
+    PrintString("3. Hoang Minh Huy - 19120241\n");
+    PrintString("---Sort program---\n");
+    PrintString("Input: n integer from user input\n");
+    PrintString("Output: n integer from input in ascending/descending order (user's choice)\n");
+    PrintString("---Ascci program---\n");
+    PrintString("Input: None\n");
+    PrintString("Output: Ascii table for printable character\n");
+    PrintString("-----------End------------\n");
+    Halt();
+}
